eb462ad4c18777fd34a20eabaf022d667f7d78e5
Robin Deits <robin.deits@gmail.com>
2016-12-28 23:58:46 -0500

    another puzzle

---
+++ test/puzzles/warrant.jl
@@ -0,0 +1,12 @@
+# http://web.mit.edu/puzzle/www/1999/puzzles/1Gumshoe/Warrants1/w1.2/w1.2.html
+@testset "warrant 1.2" begin
+    for (names, common_letter) in [
+        (["racerx", "americanmaid", "kodachi", "ladyjane"], 'a'),
+        (["brain", "judyjetson", "jonnyquest", "jeannette"], 'n'),
+        (["kenshin", "lisasimpson", "michiganjfrog", "sheila"], 'i'),
+        (["bedtimebear", "sherman", "stimpy", "mrmagoo"], 'm'),
+        (["bettyboop", "sweetpollypurebred", "skeletor", "firefly"], 'e')]
+        f = best_feature(names, 0)
+        @test f.description == "contains '$common_letter'"
+    end
+end
