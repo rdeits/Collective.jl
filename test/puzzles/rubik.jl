db391e4fa6c176797569c3780e4345496bf4510c
Robin Deits <robin.deits@gmail.com>
2017-01-04 22:14:39 -0500

    explain my crazy

--- test/puzzles/rubik.jl
+++ test/puzzles/rubik.jl
@@ -1,5 +1,5 @@
 # http://web.mit.edu/puzzle/www/2013/coinheist.com/rubik/clockwork_orange/index.html
-@testset "rubik" begin
+@testset "rubik clockwork orange meta" begin
     f = best_feature(["armoredrecon", "hypapante", "commemorativebats", "derricktruck", "brownrot", "attorneysgeneral", "sacrosanct", "impromptu"])
     @test f.description == "has at least 2 letters repeated exactly 2 times each"
 end
